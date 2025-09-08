9f529e01c58b1ce55f0bf4d7888c5fc13120525c
Angel Jose <Angel@wifi-roaming-128-4-110-180.wifi.udel.edu>
2025-09-08 11:20:08 -0400

    changes from Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import dog from "./dog.jpg"
+
 
 function App(): React.JSX.Element {
     return (
@@ -7,14 +10,56 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h2> This is header text</h2>
             <p>Angel Jose</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p>Hello World </p>
+            <p>Hello World!</p>
+      <img src={dog} alt="Dog" />
+
+            <ol>
+                <li>golden retriever </li>
+                <li>poodle </li>
+                <li>german shepard </li>
+            </ol>
+            <Button>Hello World!</Button>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                {" "}
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        First column.
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Second Column.
+                        <div
+                            style={{
+                                width: "50px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            This is <span style={{ color: "red" }}>colored text </span>
         </div>
     );
 }
-
 export default App;

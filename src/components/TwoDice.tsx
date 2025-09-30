5b67bbc6438f5736a272eba7ad3b45987a740ad7
Angel <angelj@udel.edu>
2025-09-30 11:57:30 -0400

    done

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,38 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [left, setLeft] = useState<number>(1);
+    const [right, setRight] = useState<number>(2);
+    const [message, setMessage] = useState<string>("");
+
+    const checkGame = (leftDie: number, rightDie: number) => {
+        setMessage(
+            leftDie === 1 && rightDie === 1 ? "You lose!"
+            : leftDie === rightDie ? "You win!"
+            : "",
+        );
+    };
+
+    const rollLeft = () => {
+        const goLeft = d6();
+        setLeft(goLeft);
+        checkGame(goLeft, right);
+    };
+
+    const rollRight = () => {
+        const goRight = d6();
+        setRight(goRight);
+        checkGame(left, goRight);
+    };
+
+    return (
+        <span>
+            <div> Two Dice </div>
+            <div data-testid="left-die"> {left} </div>
+            <div data-testid="right-die"> {right} </div>
+            <Button onClick={rollLeft}> Roll Left</Button>
+            <Button onClick={rollRight}> Roll Right</Button>
+            {message && <div> {message} </div>}
+        </span>
+    );
 }

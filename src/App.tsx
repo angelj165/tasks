496f134ebe5c37d54377f23cd08f356b06ee2869
Angel <angelj@udel.edu>
2025-09-30 23:25:04 -0400

    task 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import { Button, Col, Container, Row } from "react-bootstrap";
 import dog from "./dog.jpg";
-=======
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
->>>>>>> upstream/task-components
 
 function App(): React.JSX.Element {
     return (
@@ -22,7 +9,6 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
             <h1> This is header text</h1>
             <p>Angel Jose</p>
             <p>
@@ -71,28 +57,6 @@ function App(): React.JSX.Element {
                 </Row>
             </Container>
             This is <span style={{ color: "red" }}>colored text </span>
-=======
-            <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-components
         </div>
     );
 }
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
+function useDoubleHalfState() {
+    return useState<number>(10);
+}
+
+function Doubler({
+    setDhValue,
+    dhValue,
+}: {
+    setDhValue: (value: number) => void;
+    dhValue: number;
+}): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -14,7 +24,13 @@ function Doubler(): React.JSX.Element {
     );
 }
 
-function Halver(): React.JSX.Element {
+function Halver({
+    setDhValue,
+    dhValue,
+}: {
+    setDhValue: (value: number) => void;
+    dhValue: number;
+}): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -27,14 +43,15 @@ function Halver(): React.JSX.Element {
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useDoubleHalfState();
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setDhValue={setDhValue} dhValue={dhValue} /> {}
+            <Halver setDhValue={setDhValue} dhValue={dhValue} />
         </div>
     );
 }
